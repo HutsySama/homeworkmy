62ceaf5fb629a93a38499e2658999f5d3156e88b
HutsySama <fenix5982@mail.ru>
2025-07-26 02:23:05 +0300

    Add files via upload

---
+++ homework-103/javascript/main.js
@@ -0,0 +1,58 @@
+// зАДАНИЕ ПЕРВОЕ
+
+// const itsMe = {
+//     name: 'Danila',
+//     age: 21,
+//     country: 'Russia',
+//     isAdmin: false
+// }
+
+
+// зАДАНИЕ ВТОРОЕ
+
+
+// const itsMe = {
+//     name: 'Danila',
+//     age: 21,
+//     country: 'Russia',
+//     isAdmin: false,
+//     sayHello(ame) {
+//         console.log(`Hello ${ame}`)
+//     }
+// }
+// console.log(itsMe.sayHello)
+// itsMe.sayHello('Nikita')
+
+
+// зАДАНИЕ ТРЕТЬЕ
+
+
+// let notAdmin = 0
+// const users = [
+//     {
+//         name: 'Alex',
+//         age: 20,
+//         isAdmin: false
+//     },
+//     {
+//         name: 'Mike',
+//         age: 33,
+//         isAdmin: false
+//     },
+//     {
+//         name: 'Robin',
+//         age: 28,
+//         isAdmin: true
+//     },
+//     {
+//         name: 'Kai',
+//         age: 31,
+//         isAdmin: false
+//     }
+// ]
+// for (let i = 0; i < users.length; i++) {
+//     if (users[i].isAdmin === false) {
+//         notAdmin++
+//     }
+// }
+// console.log(notAdmin)
